25030d688ed32a4e3aebe674e2446240c72bd818
b117014 <b117014@iiit-bh.ac.in>
2020-04-25 21:56:22 +0530

    assignment 1

---
+++ code/MyCode/WebScrapingAssign_1.py
@@ -0,0 +1,13 @@
+import urllib.request,urllib.error,urllib.parse;
+from bs4 import BeautifulSoup
+
+data = urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_396139.html').read()
+
+soup = BeautifulSoup(data,'html.parser')
+
+span_tag = soup('span')
+sum=0
+for tag in span_tag:
+    sum+=int(tag.contents[0])
+
+print(sum)
